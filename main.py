7826cb22118a40d5beff8290a86da25e1e70a6b1
Paulo Ferraz <paulo.a.feraz@hotmail.com>
2021-02-10 19:46:25 -0300

    ''

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 # Alt + shift + e ===> run selection
 
+from os import environ
 import os
 import random
 import tweepy
 import time
 from ..credentials import *
+
+twitter_consumer_key = environ['twitter_consumer_key']
+twitter_consumer_secret = environ['twitter_consumer_secret']
+twitter_access_token = environ['twitter_access_token']
+twitter_access_token_secret = environ['twitter_access_token_secret']
 
 tw_auth = tweepy.OAuthHandler(twitter_consumer_key, twitter_consumer_secret)
 tw_auth.set_access_token(twitter_access_token, twitter_access_token_secret)
